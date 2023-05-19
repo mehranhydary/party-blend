9077dd05ff6eaa8ec36e1910e5f7f6df37b2e1c3
mehranhydary <mehranhydary@users.noreply.github.com>
2023-05-18 22:23:20 -0400

    Removing test token

--- src/Token.sol
+++ src/Token.sol
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
-
-contract Token is ERC20("Token", "TOK", 18) {
-
-}
+contract PartyBlend {}
