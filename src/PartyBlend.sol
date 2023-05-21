ccf954a68f604c5c37e7f5de6e98ce345d845a31
mehranhydary <mehranhydary@users.noreply.github.com>
2023-05-21 13:20:28 -0400

    WYA

--- src/PartyBlend.sol
+++ src/PartyBlend.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {CrowdFund} from "party-protocol/crowdfund/CrowdFund.sol";
+// Need to add the UUPSUpgradeable contract to the project when you're ready:
+// import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract PartyBlend is CrowdFund, UUPSUpgradeable {}
+// Eventually, we will build this with Party Protocol but the current version will use some of
+// the concepts we used in Bonkler Bidder and Bonkler Wallet
+
+// The goal of Party Blend is to raise NFTs and then use the NFTs on Blur's Blend Protocol
+contract PartyBlend {
+    event NftsReceived(address indexed sender);
+    event NftsWithdrawn(address indexed sender, uint256 amount);
+
+    constructor() {}
+}
--- test/PartyBlend.t.sol
+++ test/PartyBlend.t.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import {PartyBlend} from "../src/PartyBlend.sol";
 
-contract PartyBlendTest is Test {}
+contract PartyBlendTest is Test {
+    PartyBlend partyBlend;
+
+    function setUp() public {
+        partyBlend = new PartyBlend();
+    }
+}
