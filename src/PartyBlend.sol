b0861fb7064576b53b13d7a200aa359d2b79aebf
mehranhydary <mehranhydary@users.noreply.github.com>
2023-05-21 15:27:42 -0400

    Simple deposit and withdraw added for eth

--- src/PartyBlend.sol
+++ src/PartyBlend.sol
@@ -7,10 +7,36 @@ pragma solidity ^0.8.13;
 // Eventually, we will build this with Party Protocol but the current version will use some of
 // the concepts we used in Bonkler Bidder and Bonkler Wallet
 
-// The goal of Party Blend is to raise NFTs and then use the NFTs on Blur's Blend Protocol
-contract PartyBlend {
-    event NftsReceived(address indexed sender);
-    event NftsWithdrawn(address indexed sender, uint256 amount);
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/security/ReentrancyGuard.sol";
 
-    constructor() {}
+// The goal of Party Blend is to raise NFTs and then use the NFTs on Blur's Blend Protocol
+contract PartyBlend is ReentrancyGuard, Ownable {
+    event EthReceived(address indexed sender, uint256 amount);
+    event EthWithdrawn(address indexed sender, uint256 amount);
+
+    mapping(address => uint256) private ethDeposits;
+
+    constructor() {
+        transferOwnership(msg.sender);
+    }
+
+    receive() external payable {
+        ethDeposits[msg.sender] += msg.value;
+        emit EthReceived(msg.sender, msg.value);
+    }
+
+    function withdrawEth(uint256 amount) external nonReentrant {
+        require(amount <= address(this).balance, "Not enough ETH in contract");
+        require(amount <= ethDeposits[msg.sender], "Not enough ETH deposited");
+        payable(msg.sender).transfer(amount);
+        ethDeposits[msg.sender] -= amount;
+        emit EthWithdrawn(msg.sender, amount);
+    }
+
+    function getEthDepositAmount(
+        address account
+    ) external view returns (uint256) {
+        return ethDeposits[account];
+    }
 }
--- test/PartyBlend.t.sol
+++ test/PartyBlend.t.sol
@@ -10,4 +10,26 @@ contract PartyBlendTest is Test {
     function setUp() public {
         partyBlend = new PartyBlend();
     }
+
+    function test_addEth() public {
+        uint256 amount = 1000;
+        vm.prank(msg.sender);
+        (bool isTransferred, ) = address(partyBlend).call{value: amount}("");
+        assert(isTransferred);
+        assertEq(address(partyBlend).balance, amount);
+        assertEq(partyBlend.getEthDepositAmount(msg.sender), amount);
+    }
+
+    function test_removeEth() public {
+        uint256 amount = 1000;
+        vm.prank(msg.sender);
+        (bool isTransferred, ) = address(partyBlend).call{value: amount}("");
+        assert(isTransferred);
+        assertEq(address(partyBlend).balance, amount);
+        assertEq(partyBlend.getEthDepositAmount(msg.sender), amount);
+        vm.prank(msg.sender);
+        partyBlend.withdrawEth(amount);
+        assertEq(address(partyBlend).balance, 0);
+        assertEq(partyBlend.getEthDepositAmount(msg.sender), 0);
+    }
 }
